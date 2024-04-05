626b5c49d50e57175d2ccc240b178b140572ca3c
fus <nguyenphu3204@outlook.com>
2024-04-05 22:11:51 +0700

    v.0.2

--- re-def.c
+++ re-def.c
@@ -5,7 +5,22 @@ typedef unsigned int uint32_t;
 #define leftward (10111)
 #define rightward (11101)
 #define invalid_direction (101010)
+#define pin0 0
+#define pin1 1
+#define pin2 2
+#define pin3 3
+#define pin4 4
+#define pin5 5
+#define pin6 6
+#define pin7 7
+#define pin8 5
 
+void set_input_pin(uint32_t pinout){
+
+}
+void set_output_pin(uint32_t pinout){
+    
+}
 
 void turnon(uint32_t pinout, uint32_t state){
 
